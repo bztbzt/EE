3649bf06efbce92c82d28932cf669dd1ffef06ea
bzt <1784261339@qq.com>
2024-04-28 22:54:40 +0800

    first commit

---
+++ TextEE/models/__init__.py
@@ -0,0 +1,11 @@
+# from .DyGIEpp import DyGIEppE2ETrainer
+# from .OneIE import OneIEE2ETrainer
+from .CRFTagging import CRFTaggingEDTrainer, CRFTaggingEAETrainer
+# from .EEQA import EEQAEDTrainer, EEQAEAETrainer
+from .TagPrime import TagPrimeEDTrainer, TagPrimeEAETrainer
+from .TagPrimepp import TagPrimeppEDTrainer,TagPrimeppEAETrainer
+from .TagCrossAtt import TagCrossAttEAETrainer
+from .TagCrossAttRole import TagCrossAttRoleEAETrainer
+# from .Degree import DegreeE2ETrainer, DegreeEDTrainer, DegreeEAETrainer
+# from .UniST import UniSTEDTrainer
+from .PAIE import PAIEEAETrainer
---
+++ scripts/train.sh
@@ -0,0 +1,2 @@
+python TextEE/train.py -c ./config/TagCrossAttRole_EAE_phee_split1_roberta-large.jsonnet --seed 0
+python TextEE/train.py -c ./config/TagPrimepp-Con_split1_EAE_phee_roberta-large.jsonnet --seed 0
